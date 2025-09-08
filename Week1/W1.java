d91f2c63401764d33f46cd00fb21f9b068263c26
nohannah <nohannah508@gmail.com>
2025-09-08 09:09:12 +0600

    variable

---
+++ Week1/W1.java
@@ -0,0 +1,11 @@
+public class W1 {
+public static void main(String[] args) {
+   int numberOfPets = 2;
+System.out.println(numberOfPets);
+// We can combine `name` with other strings to form a sentence.
+String name = "MAC";
+System.out.println("Hi " + name + ",nice to meet you.");
+// We can also print out `numberOfPets` together with strings as follows:
+System.out.println("I have " + numberOfPets + " pets.");
+    }
+}
