53f9950dbc6bb91c59bd8bc743f41ede10a8c8fd
nohannah <nohannah508@gmail.com>
2025-09-13 11:55:40 +0600

    fact

---
+++ Week1/Factorial.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main (String [] args)
+    {
+        //Create a scanner object conencted to standard input (keyboard)
+        Scanner number= new Scanner(System.in);
+
+        System.out.println("Enter the number : ");
+        int n=number.nextInt();
+
+        int fact=1;
+        for (int i=1 ; i <=n ; i++ )
+        {
+            fact*=i;
+        }
+
+        System.out.println("Factorial of" + n + "is"+fact);
+
+        number.close();
+    }
+}
