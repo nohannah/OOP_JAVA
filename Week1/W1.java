dc85dfe2cfbc1dc4145ece1dc381b218b0d4e691
nohannah <nohannah508@gmail.com>
2025-09-13 11:06:58 +0600

    testing

--- Week1/W1.java
+++ Week1/W1.java
@@ -7,5 +7,11 @@ String name = "MAC";
 System.out.println("Hi " + name + ",nice to meet you.");
 // We can also print out `numberOfPets` together with strings as follows:
 System.out.println("I have " + numberOfPets + " pets.");
-    }
+String message="Hello world! welcome to OOP";
+int year = 2025;
+System.out.println(message + year);
+
 }
+}
+
+
---
+++ Week1/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args)
+    {
+        boolean b1=true; 
+        System.out.println("am i ok "+ b1);
+    }
+}
