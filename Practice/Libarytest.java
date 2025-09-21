e33ebbb1a07d9269a1c06e018d97f47ad02b4a38
nohannah <nohannah508@gmail.com>
2025-09-21 16:13:06 +0600

    new file for main

---
+++ Practice/Libarytest.java
@@ -0,0 +1,28 @@
+package Practice;
+
+
+public class Libarytest {
+
+    public static void main(String[] args) {
+        // Create a library with capacity 3
+        Library library = new Library(3);
+
+        // Add books
+        LibraryBook  b1= new LibraryBook("Java Basics", "Alice Smith", 29.99f);
+        LibraryBook  b2= new LibraryBook("Java Basics", "Alice Smith", 29.99f);
+        LibraryBook  b3= new LibraryBook("Python for Beginners", "Bob Johnson", 39.99f);
+        
+        library.addBook(b1);
+        library.addBook(b2);
+        library.addBook(b3);
+        // Try adding 4th book (will fail)
+        library.addBook(new LibraryBook("Extra Book", "Unknown", 15.00f));
+
+        // Display all books
+        library.displayAllBooks();
+
+        // Search for a book
+        library.searchBook("Python for Beginners");
+        library.searchBook("C++ Programming");
+    }
+}
