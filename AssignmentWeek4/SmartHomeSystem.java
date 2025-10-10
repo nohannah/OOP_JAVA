a00c3a3df73f9803776be3e089246d49ffec57ef
nohannah <nohannah508@gmail.com>
2025-10-11 00:29:52 +0600

    smart

--- AssignmentWeek4/SmartHomeSystem.java
+++ AssignmentWeek4/SmartHomeSystem.java
@@ -1,100 +1,110 @@
 package AssignmentWeek4;
-
-public class SmartDevice {
-    protected String name ; 
+// Base class
+class SmartDevice {
+    protected String name;
     protected boolean isOn;
 
-    public SmartDevice(String name)
-    {
-        this.name=name;
-        this.isOn= false;
+    public SmartDevice(String name) {
+        this.name = name;
+        this.isOn = false;
     }
-    public void turnOn()
-    {
-        isOn=true ; 
-        System.put.println(name + "is turned On.");
 
+    public void turnOn() {
+        isOn = true;
+        System.out.println(name + " is turned ON.");
     }
-     public void turnOn()
-    {
-        isOn=false ; 
-        System.put.println(name + "is turne off");
 
+    public void turnOff() {
+        isOn = false;
+        System.out.println(name + " is turned OFF.");
     }
-    public void showStatus()
-    {
 
-    System.out.println(name + " status: " + (isOn ? "ON" : "OFF"));
-
+    // Polymorphic behavior (can be overridden by subclasses)
+    public void showStatus() {
+        System.out.println(name + " status: " + (isOn ? "ON" : "OFF"));
     }
 }
-class Thermostat extends SmartDevice(String name , int temperature)
-    {
+
+// Subclass for Light
+class Light extends SmartDevice {
+    public Light(String name) {
         super(name);
-        this.temperature= temperature;
     }
-    public void setTemperature(int temp)
-    {
-        if(isOn)
-        {
-            temperature=temp;
-            System.out.println( name + "temperature set to " + temp + "Celcius");
 
-        }
-        else 
-        {
-            System.out.println(name + "is OFF, cannot set the remperature.");
-        }
+    public void dimLight() {
+        if (isOn)
+            System.out.println(name + " is dimmed to 50% brightness.");
+        else
+            System.out.println(name + " is OFF. Cannot dim.");
     }
+
     @Override
-    public void showStatus()
-    {
-    System.out.println("Thermostat \"" + name + "\" is " + (isOn ? "ON, Temp: " + temperature + "°C" : "OFF"));
- 
+    public void showStatus() {
+        System.out.println("Light \"" + name + "\" is currently " + (isOn ? "ON" : "OFF"));
+    }
+}
+
+// Subclass for Thermostat
+class Thermostat extends SmartDevice {
+    private int temperature;
+
+    public Thermostat(String name, int temperature) {
+        super(name);
+        this.temperature = temperature;
     }
 
-    class SecurityCamera extends SmartDevice
-    {
-        public SecurityCamera(String name)
-        {
-            super(name);
+    public void setTemperature(int temp) {
+        if (isOn) {
+            temperature = temp;
+            System.out.println(name + " temperature set to " + temp + "°C");
+        } else {
+            System.out.println(name + " is OFF. Cannot set temperature.");
+        }
+    }
 
-        }
-        public void recordVideo()
-        {
-            if(isOn)
-                System.out.println(name + " is recording video...");
-            else
-                System.out.println(name + " is OFF. Cannot record.");
-        }
-    
-        @Override
-        public void showStatus() {
+    @Override
+    public void showStatus() {
+        System.out.println("Thermostat \"" + name + "\" is " + (isOn ? "ON, Temp: " + temperature + "°C" : "OFF"));
+    }
+}
+
+// Subclass for Security Camera
+class SecurityCamera extends SmartDevice {
+    public SecurityCamera(String name) {
+        super(name);
+    }
+
+    public void recordVideo() {
+        if (isOn)
+            System.out.println(name + " is recording video...");
+        else
+            System.out.println(name + " is OFF. Cannot record.");
+    }
+
+    @Override
+    public void showStatus() {
         System.out.println("Security Camera \"" + name + "\" is " + (isOn ? "Active" : "Inactive"));
-        }
     }
+}
 
-    //main class
-public void SmartHomeSystem
-{
-    public static void main(String[] args)
-    {
-        SmartDevice[] devices = 
-        {
-            new Light("Living room light"),
-            new Thermostat("Main Thermostat" , 24),
-            new SecurityCamera("Front door camera")
+// Main class
+public class SmartHomeSystem {
+    public static void main(String[] args) {
+        // Polymorphic list of SmartDevices
+        SmartDevice[] devices = {
+            new Light("Living Room Light"),
+            new Thermostat("Main Thermostat", 24),
+            new SecurityCamera("Front Door Camera")
         };
-    }
 
-    //control 
-    for( SmartDevice device : devices)
-    {
-        device.turnOn();
-        device.showStatus();
-        System.out.println();
-    }
-     // Device-specific actions using type checking
+        // Control all devices using polymorphism
+        for (SmartDevice device : devices) {
+            device.turnOn();
+            device.showStatus();
+            System.out.println();
+        }
+
+        // Device-specific actions using type checking
         for (SmartDevice device : devices) {
             if (device instanceof Light) {
                 ((Light) device).dimLight();
@@ -104,7 +114,10 @@ public void SmartHomeSystem
                 ((SecurityCamera) device).recordVideo();
             }
         }
-         System.out.println("\nTurning off all devices...");
+
+        System.out.println("\nTurning off all devices...");
         for (SmartDevice device : devices) {
             device.turnOff();
+        }
+    }
 }
