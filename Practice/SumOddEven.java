f5f969ab3aad61a51a3a57a7bb311c7a191092cb
nohannah <nohannah508@gmail.com>
2025-10-14 13:56:10 +0600

    n must bigger than m

---
+++ Practice/SumOddEven.java
@@ -0,0 +1,34 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class SumOddEven {
+    public static void main(String[] args) {
+    Scanner num= new Scanner(System.in);
+    System.out.println("Enter the m number : ");
+    int m=num.nextInt();
+    System.out.println("Enter the n number : ");
+    int n = num.nextInt();
+   while (n<m)
+    {
+        System.out.println("m number must be less or equal to n ");
+         n=num.nextInt();
+    }
+    int sumEven = 0;
+    long productOdd=1; 
+    for (int i=m ; i<=n ; i++)
+    {
+        if(i%2==0)
+        {
+            sumEven+=i;
+        }
+        else 
+        {
+            productOdd*=i;
+        }
+    }
+    System.out.println("The sumof even is : " + sumEven);
+    System.out.println("Product of odd is : " + productOdd);
+    num.close();
+}
+}
